ae608fc9211ae44cd7ada6350a837db28055c7fc
luiz lopes <neutagico@gmail.com>
2024-11-20 23:39:57 -0300

    metodo da ouvidoria pesquisar por codigo

---
+++ pesquisarmanifestacoes.py
@@ -0,0 +1,9 @@
+def pesquisarporcodigo(conexao):
+    codigopesquisar = int(input("digite o codigo: "))
+    consultapesquisar = "select * from manifestacoes where codigo = %s"
+    valores = [codigopesquisar]
+    manifestacoes = listarBancoDados(conexao, consultapesquisar, valores)
+    if len(manifestacoes) > 0:
+        print(manifestacoes[0][1], "-", manifestacoes[0][2], "-", manifestacoes[0][3])
+    else:
+        print("atualmente nao temos manifestacoes nessa posicao!")
