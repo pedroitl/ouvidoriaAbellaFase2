7fe45d460a3431d4b58b4b0d1b6137bb43d0a61a
luiz lopes <neutagico@gmail.com>
2024-11-25 23:59:17 -0300

    modificando um erro de digitalizacao no metodo listagem por tipo

--- MetodosOuvidoria/listarManifestacoesPorTipo.py
+++ MetodosOuvidoria/listarManifestacoesPorTipo.py
@@ -1,15 +1,4 @@
-def listarManifestacoes(conexao):
-
-    lista = listarBancoDados(conexao, "select * from manifestacoes")
-
-<<<<<<< HEAD:listarmanifestacoesportipo.py
-    if len(lista) > 0:
-            print("lista das manifestacoes:")
-            for item in lista:
-                print(item[0], "-", item[1], "-", item[2], "-", item[3])
-    else:
-            print("nao existem manifestacoes a serem exibidas!")
-=======
+def listarManifestacoesPorTipo(conexao):
     tipomanifestacao = input("Digite o tipo de manifestacao a ser cadastrada: ")
     consultalistagemtipo = "select * from manifestacoes where tipo = %s"
     tipo = [tipomanifestacao]
@@ -20,4 +9,4 @@ def listarManifestacoes(conexao):
             print(item[0], "-", item[1], "-", item[2], "-", item[3])
     else:
         print("Nao temos nenhuma manifestacao nesse tipo!")
->>>>>>> d65135ad0356eb30c4c6307fec50be4558f2a935:MetodosOuvidoria/listarManifestacoesPorTipo.py
+
